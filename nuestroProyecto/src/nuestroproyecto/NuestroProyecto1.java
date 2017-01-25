015acdfd17f568688a37e4c72c4c9611efd65e65
a023315568p <a023315568p@s09-pc20>
2017-01-25 12:54:44 +0100

    hola

--- nuestroProyecto/src/nuestroproyecto/NuestroProyecto1.java
+++ nuestroProyecto/src/nuestroproyecto/NuestroProyecto1.java
@@ -17,6 +17,7 @@ public class NuestroProyecto1{
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("HOLA QUE ASE");
+        System.out.print("HOLA QUE ASE");
     }
     
 
